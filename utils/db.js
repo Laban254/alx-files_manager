e25913384313bdde9fac37ec57a0b55a7a89684e
laban254 <labanrotich6544@gmail.com>
2023-09-05 10:05:31 -0700

    MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,44 +1,40 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-  /**
-   * Initializes a new instance of DBClient
-   */
   constructor() {
-    const HOST = process.env.DB_HOST || 'localhost';
-    const PORT = process.env.BD_PORT || 27017;
-    const DATABASE = process.env.DB_DATABASE || 'files_manager';
-    const URI = `mongodb://${HOST}:${PORT}`;
-    this.mongoClient = new MongoClient(URI, { useUnifiedTopology: true });
-    this.mongoClient.connect((error) => {
-      if (!error) this.db = this.mongoClient.db(DATABASE);
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    this.client.connect();
+    this.db = this.client.db(this.database);
   }
 
-  /**
-   * Check mongodb client's connection status
-   * @returns {boolean} mongoClient connection status
-   */
   isAlive() {
-    return this.mongoClient.isConnected();
+    return !!this.client && this.client.isConnected();
   }
 
   async nbUsers() {
-    const usersCollection = this.getCollection('users');
+    if (!this.isAlive()) return 0;
+
+    const usersCollection = this.db.collection('users');
     const numberOfUsers = await usersCollection.countDocuments();
     return numberOfUsers;
   }
 
-  /**
-   * Queries 'files' collection
-   * @returns {number} - number of documents in files collection
-   */
   async nbFiles() {
-    const filesCollection = this.getCollection('files');
-    const numberOfFiles = filesCollection.countDocuments();
+    if (!this.isAlive()) return 0;
+
+    const filesCollection = this.db.collection('files');
+    const numberOfFiles = await filesCollection.countDocuments();
     return numberOfFiles;
   }
 }
 
 const dbClient = new DBClient();
+
 export default dbClient;
+
